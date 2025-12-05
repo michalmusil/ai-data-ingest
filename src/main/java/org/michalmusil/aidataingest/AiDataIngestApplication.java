49dcd147941fcbf6f4b786a23723c9808fc8e255
Michal Musil <musilmichal16@gmail.com>
2025-12-05 09:32:21 +0100

    feat: add initial project structure

---
+++ src/main/java/org/michalmusil/aidataingest/AiDataIngestApplication.java
@@ -0,0 +1,13 @@
+package org.michalmusil.aidataingest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiDataIngestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AiDataIngestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/michalmusil/aidataingest/AiDataIngestApplicationTests.java
@@ -0,0 +1,13 @@
+package org.michalmusil.aidataingest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiDataIngestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
